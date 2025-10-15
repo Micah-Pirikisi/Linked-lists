2b7f061bdb82d3686d3c637990c1313fff346477
Micah-Pirkisi <micahpirikisi@gmail.com>
2025-10-15 10:07:02 +0200

    Created insertAt() function

--- index.js
+++ index.js
@@ -31,4 +31,37 @@ class LinkedList {
 
         current.next = newNode; 
     }
+
+    size() {
+        let count = 0; 
+        let current = this.head; 
+        while(current) {
+            count++; 
+            current = current.next;
+        }
+
+        return count; 
+    }
+
+    insertAt(index, value) {
+        if(index < 0 || index > this.size()) {
+            console.error('Invalid index'); 
+            return; 
+        }
+
+        const newNode = new Node(value); 
+        if(index === 0) {
+            newNode.next = this.head; 
+            this.head = newNode; 
+            return; 
+        }
+
+        let current = this.head; 
+        for(let i = 0; i < index -1; i++) {
+            current = current.next; 
+        }
+
+        newNode.next = current.next; 
+        current.next = newNode; 
+    }
 }
