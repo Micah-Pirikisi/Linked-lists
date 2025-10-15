039bc33b0a90d08974d1484e98af820da4d08b1c
Micah-Pirkisi <micahpirikisi@gmail.com>
2025-10-15 08:12:13 +0200

    Initial commit: Created linkedList and Node classes

---
+++ index.js
@@ -0,0 +1,12 @@
+class Node {
+    constuctor(data) {
+        this.data = data; 
+        this.next = null; 
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null; 
+    }
+}
